73dba7fd02d7bb183b03e6b401cdefb6d11841d8
Shahab Asif <shahabasif@hotmail.com>
2023-11-29 17:50:26 +0000

    'pallendrome_code_addition'

---
+++ Pallendrome_code.py
@@ -0,0 +1,4 @@
+
+#Code to Check whether the string is Pallendrome or not 
+string = input("Enter the String: ")
+print("string is Pallendrome") if string ==string[::-1] else print("string is not Pallendrome")
