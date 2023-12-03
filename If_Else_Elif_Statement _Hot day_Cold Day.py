b9b48fc30d82ea857df73e8a8c8725ff879e4f08
Shahab Asif <shahabasif@hotmail.com>
2023-12-03 15:03:46 +0000

    'if_else_elif_statement'

---
+++ If_Else_Elif_Statement _Hot day_Cold Day.py
@@ -0,0 +1,15 @@
+#If Else Elif Statement (Hot day and Cold Day)
+
+
+is_hot = False
+is_cold = False  
+
+if is_hot:
+    print('Its a Hot Day')
+    print('Drink plenty of water')
+elif is_cold: 
+    print('Its a cold day')
+    print('Wear warm clothes')
+else: 
+    print('Its a lovely day')
+print('Enjoy your Day')
